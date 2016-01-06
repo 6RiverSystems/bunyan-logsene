b59e994b65883b50ed3627344f50be9269809e6f
Joshua Pollak <josh@offthehill.org>
2016-01-06 17:20:23 -0500

    first pass, but functional

--- index.js
+++ index.js
@@ -1,78 +1,54 @@
 var Writable = require('stream').Writable;
 var domain = require('domain');
 var util = require('util');
-var elasticsearch = require('elasticsearch');
-var moment = require('moment');
+var Logsene = require('logsene-js');
 
 var levels = {
-  10: 'trace',
-  20: 'debug',
-  30: 'info',
-  40: 'warn',
-  50: 'error',
-  60: 'fatal'
+	10: 'trace',
+	20: 'debug',
+	30: 'info',
+	40: 'warn',
+	50: 'error',
+	60: 'fatal'
 };
 
-function generateIndexName (pattern, entry) {
-  return moment.utc(entry.timestamp).format(pattern);
+function LogseneStream(options) {
+	options = options || {};
+	this._client = options.client ||
+		new Logsene(options.token,
+		options.type || 'bunyan_logsene',
+		options.url);
+
+	Writable.call(this, options);
 }
 
-function callOrString (value, entry) {
-  if (typeof(value) === 'function') {
-    return value(entry);
-  }
-  return value;
-}
+util.inherits(LogseneStream, Writable);
 
-function ElasticsearchStream (options) {
-  options = options || {};
-  this._client = options.client || new elasticsearch.Client(options);
-  this._type = options.type || 'logs';
-  var indexPattern = options.indexPattern || '[logstash-]YYYY.MM.DD';
-  this._index = options.index || generateIndexName.bind(null, indexPattern); 
-  Writable.call(this, options);
-}
+LogseneStream.prototype._write = function(entry, encoding, callback) {
 
-util.inherits(ElasticsearchStream, Writable);
+	var client = this._client;
 
-ElasticsearchStream.prototype._write = function (entry, encoding, callback) {
+	var d = domain.create();
+	d.on('error', function(err) {
+		console.log('Logsene Error', err.stack);
+	});
+	d.run(function() {
+		entry = JSON.parse(entry.toString('utf8'));
 
-  var client = this._client;
-  var index = this._index;
-  var type = this._type;
+		// Reassign these fields so them match what Logsene expects
+		// expects to see.
+		entry['@timestamp'] = entry.time;
+		entry.level = levels[entry.level];
+		entry.message = entry.msg;
+		entry.host = entry.hostname;
 
-  var d = domain.create();
-  d.on('error', function (err) { 
-    console.log("Elasticsearch Error", err.stack);
-  });
-  d.run(function () {
-    entry = JSON.parse(entry.toString('utf8'));
-    var env = process.env.NODE_ENV || 'development';
+		// remove duplicate fields
+		delete entry.time;
+		delete entry.msg;
+		delete entry.hostname;
 
-    // Reassign these fields so them match what the default Kibana dashboard 
-    // expects to see.
-    entry['@timestamp'] = entry.time;
-    entry.level = levels[entry.level];
-    entry.message = entry.msg;
-
-    // remove duplicate fields
-    delete entry.time;
-    delete entry.msg;
-
-    var datestamp = moment(entry.timestamp).format('YYYY.MM.DD');
-
-    var options = {
-      index: callOrString(index, entry),
-      type: callOrString(type, entry),
-      body: entry
-    };
-
-    client.create(options, function (err, resp) {
-      if (err) console.log('Elasticsearch Stream Error:', err.stack);
-      callback();
-    });
-
-  });
+		client.log(entry.level, entry.message, entry, callback);
+	});
 };
 
-module.exports = ElasticsearchStream;
+module.exports = LogseneStream;
